b30333b062ddb8a12cb71625a61d5db8f06b6ea9
Аня Смирнова <annedadaa@RN-145-97-167-84.eduroam.rug.nl>
2024-04-03 13:33:04 +0200

    Update annotations structure, code refactoring

--- translate.py
+++ translate.py
@@ -1,8 +1,5 @@
-import pandas as pd
-import numpy as np
-import pprint
 from transformers import M2M100ForConditionalGeneration, M2M100Tokenizer
-from datasets import load_from_disk, concatenate_datasets
+from datasets import load_from_disk, concatenate_datasets, Dataset
 import os
 import argparse
 
@@ -18,7 +15,9 @@ def create_arg_parser():
     return args
 
 
-def en_to_lang(model, tokenizer, en_sentence, lang):
+def en_to_lang(model, tokenizer, en_sentence: str, lang: str) -> str:
+
+    """Translate English context sentences to the target language"""
 
     tokenizer.src_lang = "en"
     encoded_zh = tokenizer(en_sentence, return_tensors="pt")
@@ -34,27 +33,34 @@ def en_to_lang(model, tokenizer, en_sentence, lang):
     return fixed_result
 
 
-def load_dataset():
-    dataset_dirs = ['annotations/'+path for path in os.listdir('annotations/') if not path.endswith('.csv')]
-    dataset = concatenate_datasets([load_from_disk(dataset_path) for dataset_path in dataset_dirs])
+def load_dataset(lang: str) -> Dataset:
+
+    """Load dataset"""
+
+    dataset_path = f'annotations/annotations_{lang}'
+    dataset = load_from_disk(dataset_path)
     return dataset
 
 
-def add_translations(row, model, tokenizer, lang):
+def add_translations(row: dict, model, tokenizer, lang: str) -> dict:
+
+    """Add translated context sentences to the dataset"""
+
     translated_sentence = [en_to_lang(model=model, tokenizer=tokenizer, en_sentence=s, lang=lang) for s in row['context']['en']] 
     row['context'][lang] = translated_sentence
     return row
 
 if __name__ == '__main__':
+    
     args = create_arg_parser()
 
-    dataset = load_dataset()
+    dataset = load_dataset(lang=args.lang)
     
     model = M2M100ForConditionalGeneration.from_pretrained("facebook/m2m100_418M")
     tokenizer = M2M100Tokenizer.from_pretrained("facebook/m2m100_418M")
     
     dataset = dataset.map(add_translations, 
-                                      fn_kwargs={'model': model, 'tokenizer': tokenizer, 'lang': args.lang})
+                          fn_kwargs={'model': model, 'tokenizer': tokenizer, 'lang': args.lang})
     
     dataset.save_to_disk(f'{args.save_path}/')
     dataset.to_csv(f'{args.save_path}.csv')
